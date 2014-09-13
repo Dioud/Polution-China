8223b9f681933b5970b7165f2d2a010c2162b6a3
charles <charles@gitcafe.com>
2014-09-14 00:58:26 +0800

    add rescue

--- app/models/aqi.rb
+++ app/models/aqi.rb
@@ -1,11 +1,17 @@
 class Aqi
   def self.all
     result={}
-    cities = %w(tianjin beijing shanghai hongkong xingtai kunming haerbin guanzhou nanjing)
+    cities = %w(beijing tianjin shanghai hongkong xingtai kunming haerbin guangzhou nanjing)
     cities.each do |city|
-      doc = Nokogiri::HTML(RestClient.get("http://aqicn.org/city/#{city}/").to_str)
-      res= doc.css('div.aqivalue')[0].content rescue 'error'
-      result[city] = res
+      begin
+        p city
+        doc = Nokogiri::HTML(RestClient.get("http://aqicn.org/city/#{city}/").to_str)
+        res= doc.css('div.aqivalue')[0].content
+      rescue
+        res = 0
+      end
+      result[city.humanize] = res
     end
+    result
   end
 end
